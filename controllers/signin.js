1a6a15d5a6ef08b7e841d113df8be9f9aeb9e554
unknown <Jonipux@yandex.ru>
2023-05-21 02:16:34 +0300

    add json result sign-in

--- controllers/signin.js
+++ controllers/signin.js
@@ -29,7 +29,7 @@ module.exports.signin = (req, res, next) => {
             httpOnly: true,
             sameSite: 'none',
             secure: true,
-          }).end({});
+          }).end({ message: 'Авторизация прошла успешно!' });
         })
         .catch(next);
     })
