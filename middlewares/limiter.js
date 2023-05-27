9f12cce97c3d2c61c63c73c2509c5cc568f09c3f
unknown <Jonipux@yandex.ru>
2023-05-27 17:19:16 +0300

    изменил кол-во запросов на api в единицу времени

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit(
   {
     windowMs: 5 * 60 * 1000,
-    max: 200,
+    max: 1000,
     standardHeaders: true,
     legacyHeaders: false,
   },
